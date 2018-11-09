b3606481c3c76dba56f8a097314ffe80cc35cd9e
eipc16 <pprzemek.312@gmail.com>
2018-11-09 11:43:08 +0100

    added inputs for filtering tables

--- scripts/gamedata.js
+++ scripts/gamedata.js
@@ -4,30 +4,28 @@ var data =
     "Title": "SimCity",
     "Screenshot_1": "games/simcity.jpg",
     "Screenshot_2": "games/simcity2.jpg",
-    "Description": "
-                  
-                   ",
-    "Download": "http://www.google.com",
-    "Rating": 4,
-    "Year": 1997,
-    "Developer": "Dev_1",
-    "Type": "Turowe",
-    "Genre": "Gry strategiczne"
-  },
-  {
-    "Title": "1",
-    "Screenshot_1": "games/simcity.jpg",
-    "Screenshot_2": "",
-    "Description": "Good",
-    "Download": "http://www.google.com",
-    "Rating": 5,
-    "Year": 1992,
-    "Developer": "Dev_2",
+    "Description": "W grze wcielamy się w burmistrza, którego zadaniem jest zwykła rozbudowa miasta lub osiąganie celów w wybranym scenariuszu rozgrywki. W zależności od wybranego poziomu trudności gracz na początku dysponuje odpowiednią ilością gotówki, którą może przeznaczać na kupno działek, budowę domów mieszkalnych, fabryk itp. Gracz poprzez zaspokajanie podstawowych potrzeb (dostęp do prądu, miejsca pracy) oraz eliminacje przestępczości czy niwelowanie korków ulicznych ma wpływ na zadowolenie mieszkańców co z kolei przekłada się na możliwość ściągania podatków. Rozgrywka w <mark>SimCity</mark> polega na jak najdłuższym przetrwaniu twojego miasta.",
+    "Download": "ftp://sunsite.icm.edu.pl/pub/vim/pc/vim81w32.zip",
+    "Rating": 7,
+    "Year": 1993,
+    "Developer": "Maxis",
     "Type": "RTS",
     "Genre": "Gry strategiczne"
   },
   {
-    "Title": "2",
+    "Title": "Heroes of Might & Magic III",
+    "Screenshot_1": "games/heroes.jpg",
+    "Screenshot_2": "",
+    "Description": "<mark>Heroes of Might & Magic III</mark> to trzecia część serii, która znalazła najwyższe uznanie wśród graczy. Podobnie jak w poprzednich częściach, tutaj również kierujemy poczynaniami bohaterów, zdobywamy, rozbudowujemy i fortyfikujemy miasta. Bohaterowie, przemierzający krainę wraz ze swoimi wojskami, odkrywają nowe terytoria, szukają magicznych artefaktów i skarbów, oraz prowadzą armie do boju. Możemy rozegrać pojedyncze scenariusze, całe kampanie zawierające wiele misji lub scenariusze do gry wieloosobowej. Można również wykonać własny scenariusz za pomocą dołączonego edytora.",
+    "Download": "https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.46.172.exe",
+    "Rating": 9,
+    "Year": 1999,
+    "Developer": "New World Computing",
+    "Type": "Turowe",
+    "Genre": "Gry strategiczne"
+  },
+  {
+    "Title": "Test 1",
     "Screenshot_1": "games/simcity.jpg",
     "Screenshot_2": "games/heroes.jpg",
     "Description": "Good",
@@ -37,7 +35,31 @@ var data =
     "Developer": "Dev_3",
     "Type": "2D",
     "Genre": "Gry platformowe"
-  }
+  },
+  {
+    "Title": "Test 2",
+    "Screenshot_1": "games/simcity.jpg",
+    "Screenshot_2": "games/heroes.jpg",
+    "Description": "Good",
+    "Download": "http://www.google.com",
+    "Rating": 8,
+    "Year": 1997,
+    "Developer": "Dev_3",
+    "Type": "2D",
+    "Genre": "Gry platformowe"
+  },
+  {
+    "Title": "Test 3",
+    "Screenshot_1": "games/simcity.jpg",
+    "Screenshot_2": "games/heroes.jpg",
+    "Description": "Good",
+    "Download": "http://www.google.com",
+    "Rating": 8,
+    "Year": 1997,
+    "Developer": "Dev_3",
+    "Type": "RTS",
+    "Genre": "Gry strategiczne"
+  },
 ];
 
 var game_types = [
--- scripts/tables.js
+++ scripts/tables.js
@@ -1,25 +1,78 @@
-var HTML_text = "";
 
-for (var i = 0; i < data.length; i++) {
-  HTML_text += "<div><table id=\"" + data[i]["Title"] + "\" class=\"gametable\""
-            + "<tr><td colspan=\"2\"><h2>" + data[i]["Title"] + "</h2></td></tr>"
-            + "<tr><td class=\"gamedesc\"><h3>Opis</h3><div class=\"gameparagraph\"><p>" + data[i]["Description"] + "</p></div></td>"
-            + "<td><table><tr><th>" + data[i]["Title"] + "</th></tr>"
-            + "<tr><td><div class=\"gamedetails\">"
-            + "<img id=\"screen1\" src=\"" + data[i]["Screenshot_1"] + "\" width=\"232\" height=\"131\" alt=\"Screenshot\">";
-  if(data[i]["Screenshot_2"].length > 0)
-    HTML_text += "<img id=\"screen2\" src=\"" + data[i]["Screenshot_2"] + "\" width=\"232\" height=\"131\" alt=\"Screenshot\">";
+function drawTables(genre, type, name="") {
+  var HTML_text = "";
 
-  HTML_text += "<p><strong>Ocena</strong></p>"
-            + "<meter value=\"" + data[i]["Rating"] + "\" min=\"0\" max=\"10\" class=\"gamemeter\">" + data[i]["Rating"] + " na 10</meter>"
-            + "<p><a href=\"" + data[i]["Download"] + "\">Download</a></p>"
-            + "<details><summary>Szczegóły</summary>"
-            + "<p><strong>Rok wydania</strong></p>"
-            + "<p>" + data[i]["Year"] + "</p>"
-            + "<p><strong>Producent</strong></p>"
-            + "<p>" + data[i]["Developer"] + "</p>"
-            + "</details><br><br>"
-            + "</div></td></tr></table></td></tr></table></td></tr></table></div>";
+  for (var i = 0; i < data.length; i++) {
+    correctGenre = data[i]["Genre"] === genre || genre === "Wszystkie";
+    correctType = data[i]["Type"] === type || type === "Wszystkie";
+    correctName = data[i]["Title"].includes(name) || name.length === 0;
+    if(correctGenre && correctType && correctName){
+      HTML_text += "<div><table id=\"" + data[i]["Title"] + "\" class=\"gametable\""
+                + "<tr><td colspan=\"2\"><h2>" + data[i]["Title"] + "</h2></td></tr>"
+                + "<tr><td class=\"gamedesc\"><h3>Opis</h3><div class=\"gameparagraph\"><p>" + data[i]["Description"] + "</p></div></td>"
+                + "<td><table><tr><th>" + data[i]["Title"] + "</th></tr>"
+                + "<tr><td><div class=\"gamedetails\">"
+                + "<img id=\"screen1\" src=\"" + data[i]["Screenshot_1"] + "\" width=\"232\" height=\"131\" alt=\"Screenshot\">";
+      if(data[i]["Screenshot_2"].length > 0)
+        HTML_text += "<img id=\"screen2\" src=\"" + data[i]["Screenshot_2"] + "\" width=\"232\" height=\"131\" alt=\"Screenshot\">";
+
+      HTML_text += "<p><strong>Ocena</strong></p>"
+                + "<meter value=\"" + data[i]["Rating"] + "\" min=\"0\" max=\"10\" class=\"gamemeter\">" + data[i]["Rating"] + " na 10</meter>"
+                + "<p><a href=\"" + data[i]["Download"] + "\">Download</a></p>"
+                + "<details><summary>Szczegóły</summary>"
+                + "<p><strong>Rok wydania</strong></p>"
+                + "<p>" + data[i]["Year"] + "</p>"
+                + "<p><strong>Producent</strong></p>"
+                + "<p>" + data[i]["Developer"] + "</p>"
+                + "</details><br><br>"
+                + "</div></td></tr></table></td></tr></table></td></tr></table></div>";
+    }
+  }
+
+  document.getElementById('table-container').innerHTML = HTML_text;
 }
 
-document.write(HTML_text);
+function loadGenres() {
+  var options = '';
+
+  for (var i = 0; i < game_types.length + 1; i++){
+    if (i === 0)
+      genre = "Wszystkie";
+    else {
+      genre = game_types[i - 1]["Genre"];
+    }
+    options += "<option value=\"" + genre + "\">" + genre + "</option>";
+  }
+
+  document.getElementById('genre').innerHTML = options;
+}
+
+function loadTypes(genre="Wszystkie") {
+  var options = "<option value=\"Wszystkie\">Wszystkie</option>";
+
+  for (var i = 0; i < game_types.length; i++){
+    if (genre === game_types[i]["Genre"] || genre === "Wszystkie"){
+      console.log("Selected: " + genre + " Current: " + game_types[i]["Genre"]);
+      type_array = game_types[i]["Types"];
+      console.log(type_array)
+      for (var j = 0; j < type_array.length; j++){
+        options += "<option value=\"" + type_array[j] + "\">" +  type_array[j] + "</option>";
+      }
+    }
+  }
+
+  document.getElementById('type').innerHTML = options;
+
+  updateTables();
+}
+
+function updateTables(name="") {
+
+  genre_selector = document.getElementById('genre');
+  genre = genre_selector.options[genre_selector.selectedIndex].value;
+
+  type_selector = document.getElementById('type');
+  type = type_selector.options[type_selector.selectedIndex].value;
+
+  drawTables(genre, type, name);
+}
