d92cdf81c1cabfab87840618f73bb6bb54968456
eipc16 <pprzemek.312@gmail.com>
2018-11-12 10:27:32 +0100

    used window.addEventListener for accesing simplegame.html (5 cliks in 1 sec)

---
+++ scripts/init_game.js
@@ -0,0 +1,16 @@
+clicks = 0;
+
+window.onload = function() {
+  window.addEventListener("click", function() {
+    clicks++;
+
+    console.log('Cliks: ' + clicks);
+    if(clicks > 5) {
+      window.location.pathname = '/simplegame.html';
+    }
+  });
+}
+
+var interval = setInterval(function () {
+  clicks = 0;
+}, 1000);
