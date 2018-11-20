d593c7b4f0a898aee599bc9004e2ee464968c6e7
eipc16 <pprzemek.312@gmail.com>
2018-11-20 22:00:51 +0100

    update

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ createTextNode (OK), appendChild (OK), insertBefore (OK),
 replaceChild (OK), removeChild (OK) oraz właściwości parentNode (OK).
 2. Zademonstrować działanie kolekcji: images, links, forms, anchors
 oraz metod item i namedItem.
-3. Umożliwić użytkownikowi zmianę na żądanie stylów CSS: koloru tła, koloru tekstu,
-rodzaju czcionki (z listy).
-4. Zademonstrować możliwości właściwości altKey, ctrlKey, shiftKey
-keyCode, clientX, clientY, screenX, screenY obiektu event.
-Wykorzystać zdarzenia: mousemove, mousedown, mouseover, mouseout.
+3. Umożliwić użytkownikowi zmianę na żądanie stylów CSS: koloru tła (OK), koloru tekstu (OK),
+rodzaju czcionki (z listy) (OK).
+4. Zademonstrować możliwości właściwości altKey (OK), ctrlKey (OK), shiftKey (OK)
+keyCode, clientX (OK), clientY (OK), screenX (OK), screenY (OK) obiektu event.
+Wykorzystać zdarzenia: mousemove (OK), mousedown (OK), mouseover, mouseout.
 5. W formularzach wykorzystać zdarzenia focus i blur do wyświetlania tekstów
 pomocy oraz zdarzenia submit i reset do wyzwalania okien potwierdzających.
 
--- scripts/simplegame.js
+++ scripts/simplegame.js
@@ -1,88 +1,3 @@
-var red_prob = 0.0;
-var input_number = 0;
-
-function getNumber() {
-  do {
-    input = window.prompt("Wpisz ile pól chcesz wygenerować", "50");
-    input_num = parseInt(Math.floor(input));
-  } while (isNaN(input_num));
-
-  return input_num;
-}
-
-function getProbability() {
-  while (red_prob < 0.2 || red_prob > 0.8) {
-   input = window.prompt("Wpisz prawdopodobieństwo wystąpienia czerwonego pola [0.2, 0.8]", "0.5");
-   input_prob = parseFloat(input);
-   if(!isNaN(input_prob)) {
-     red_prob = input_prob;
-   }
-  }
-}
-
-window.onload = function() {
-  var button = document.getElementById('startButton');
-
-  button.addEventListener("click", function() {
-    input_number = getNumber();
-    getProbability();
-
-    var prob = Math.random();
-    var red = true;
-    var number = document.createElement('div');
-    var numbers_field = document.getElementById('numbers');
-    numbers_field.innerHTML = '';
-
-    number.setAttribute("class", "number-class-black");
-    number.setAttribute("id", "number-container");
-
-    var result = 'Na ' + input_number + ' liczb z prawdopodobieństwem wylosowania czerwonej ' + red_prob + ' udalo Ci sie wylosowac ';
-
-    if (prob > red_prob) {
-      red = false;
-    }
-
-    if (red) {
-      number.setAttribute("class", "number-class-red");
-      result += ' CZERWONA liczbe!';
-    } else {
-      result += ' CZARNA liczbe!';
-    }
-
-    numbers_field.appendChild(number);
-
-    window.alert(result);
-
-    red_prob = 0.0;
-    input_number = 0;
-  });
-
-  document.getElementById('testButton').addEventListener("click", function() {
-    result = window.prompt('Podaj dowolny ciąg znaków!');
-    type = 0 // 0 - string, 1 - float, 2 - int
-
-    if(!isNaN(parseFloat(result))){
-      type = 2;
-    }
-
-    if(!isNaN(parseInt(result)) && (parseFloat(result) - parseInt(result)) === 0) {
-      type = 1;
-    }
-
-    switch(type) {
-      case 1:
-        document.writeln('Wpisales liczbe calkowita!');
-        break;
-      case 2:
-        document.writeln('Wpisales liczbe rzeczywista!');
-        break;
-      default:
-        document.writeln('Wpisales Stringa!');
-        break;
-    }
-  })
-};
-
 function isKeyPressed(event) {
   if (event.altKey && event.shiftKey && !event.ctrlKey) {
     window.alert('Wcisnales razem SHIFT i ALT');
@@ -101,18 +16,31 @@ function savePosition(event) {
     if(history.childNodes.length < 1)
       history.appendChild(el);
     else
-      if(history.childNodes.length > 4)
+      if(history.childNodes.length > 8){
         history.removeChild(history.lastChild);
-      history.insertBefore(el, history.firstChild);
+        history.removeChild(history.lastChild);
+      }
+    history.insertBefore(document.createElement('br'), history.firstChild);
+    history.insertBefore(el, history.firstChild);
+
+
+    console.log(history);
 }
 
 function updatePosition(event) {
   var div = document.getElementById('mousetracker')
 
-  var coords = document.createTextNode('\nClient X: ' + event.clientX + ' | Client Y: ' + event.clientY + ' ||| Screen X: ' + event.screenX + ' | Screen Y: ' + event.screenY + '\n');
+  var coords = document.createTextNode('Client X: ' + event.clientX + ' | Client Y: ' + event.clientY + ' ||| Screen X: ' + event.screenX + ' | Screen Y: ' + event.screenY);
 
   if(div.childNodes.length < 1)
     div.appendChild(coords);
   else
     div.replaceChild(coords, div.firstChild);
+}
+
+function setStyle() {
+  background = document.getElementById('backgroundcolor').value
+  color = document.getElementById('fontcolor').value
+  size = document.getElementById('fontsize').value
+  document.body.setAttribute("style", "background-color: " + background + ";color: " + color + ";font-size: " + size);
 }
