99445110809f51f8b761389b7c90a43b31d75cb6
eipc16 <pprzemek.312@gmail.com>
2018-12-13 16:00:06 +0100

    fixed login

--- navigation.php
+++ navigation.php
@@ -22,18 +22,19 @@
 <a href="about.html">
   <img src="buttons/about.png" id="about" alt="Something about us">
 </a>
-<form class="login" method="post" action="login.php" <?php if(isset($_SESSION['user_id'])) echo 'hidden';?>>
+<div id="login">
+<form method="post" action="login.php" <?php if(isset($_SESSION['user_id'])) echo 'hidden';?>>
   <input type="hidden" name="target" value="index.html" />
   <input type="hidden" name="action" value="login"/>
   <input type="text" name="user"/>
   <input type="password" name="password"/>
   <input type="submit" value="Zaloguj się!"/>
 </form>
-<form class="login" method="post" action="login.php" <?php if(!isset($_SESSION['user_id'])) echo 'hidden';?>>
+<form method="post" action="login.php" <?php if(!isset($_SESSION['user_id'])) echo 'hidden';?>>
   <input type="hidden" name="target" value="index.html" />
   <input type="hidden" name="action" value="logout"/>
   <div>Użytkownik: <?php echo $_SESSION['user_id'] ?> </div><br>
   <input type="submit" value="Wyloguj się!"/>
 </form>
-
+</div>
 </html>
